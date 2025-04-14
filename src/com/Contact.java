f9324636dacbffb08d122e9b2f337e2054b7d7d3
poojangit <poojang147@gmail.com>
2025-04-14 20:27:40 +0530

    [POOJA] Added :  Added functionality to add a new Contact to Address Book using console input and Object-Oriented Programming

--- src/com/Contact.java
+++ src/com/Contact.java
@@ -13,14 +13,14 @@ public class Contact {
 
     // Constructor
     public Contact(String firstName, String lastName, String address, String city,
-            String state, int zip, long phoneNumber, String email) {
+            String state, int zip2, long phoneNumber2, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
+        this.zip = zip2;
+        this.phoneNumber = phoneNumber2;
         this.email = email;
     }
 
@@ -89,18 +89,16 @@ public class Contact {
         this.email = email;
     }
 
-    // To display the contact details
-    @Override
-    public String toString() {
-        return "--------Contacts -------- :   " +
-                "[firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip='" + zip + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                ']';
+    // Method to display contact details
+    public void displayContact() {
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("ZIP: " + zip);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Email: " + email);
     }
+
 }
