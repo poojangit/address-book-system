4ee51273a7452ff9d98e7cca6f298d479310d49f
poojangit <ramireddydeepika@gmail.com>
2025-04-14 19:33:27 +0530

    [POOJA] Added : created a new class - Contacts by adding the contact details and methods - created a contact object in main class

--- src/com/AddressBookSystem.java
+++ src/com/AddressBookSystem.java
@@ -3,10 +3,18 @@ package com;
 public class AddressBookSystem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book program");
-		// Create a new contact object
-		Contact contact = new Contact("Pooja", "N G", "Perdoor", "Udupi", "Karnataka", 576124, 7899340021L,
-				"poojang241@gmail.com");
-		// printing contact details
+		// Create a object - contact
+		Contact contact = new Contact(
+				"Pooja",
+				"N G",
+				"Perdoor",
+				"Udupi",
+				"Karnataka",
+				576453,
+				736365352L,
+				"poojahd662@gmail.com");
+
+		// Display the contact
 		System.out.println(contact);
 	}
 }
